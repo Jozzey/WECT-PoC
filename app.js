67d4c33ad281eacc7f5ca3ecf6c3a7586f836a37
Jozzey <jozzey@hotmail.com>
2019-12-23 17:14:44 +0000

    Added condition for when to update file

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const auth = require('./auth');
 const graph = require('./graph');
+const updateRequired = require('./test');
 const fs = require('fs');
 const config = require('./config');
 
-// Get an access token for the app.
-auth.getAccessToken().then(function (token) {
-  // Get all of the data from the list.
-  graph.getData(token)
-    .then(function (data) {
-      // Write the list data to a file
-      fs.writeFile(`${config.filePath}${config.fileName}`, JSON.stringify(data), function (err) {
-        if (err) throw err;
-        console.log('Data saved!');
+// Check to se if the file need to be updated
+if (updateRequired.checkData()) {
+  // Get an access token for the app.
+  auth.getAccessToken().then(function (token) {
+    // Get all of the data from the list.
+    graph.getData(token)
+      .then(function (data) {
+        // Write the list data to a file
+        fs.writeFile(`${config.filePath}${config.fileName}`, JSON.stringify(data), function (err) {
+          if (err) throw err;
+          console.log('Data saved!');
+        });
+      }, function (error) {
+        console.error('>>> Error getting list data: ' + error);
       });
-    }, function (error) {
-      console.error('>>> Error getting list data: ' + error);
-    });
-}, function (error) {
-  console.error('>>> Error getting access token: ' + error);
-});
+  }, function (error) {
+    console.error('>>> Error getting access token: ' + error);
+  });
+} else console.log('No need to update data');
--- updateRequired.js
+++ updateRequired.js
@@ -1,41 +1,24 @@
-const Q = require('q');
-const config = require('./config');
 const fs = require('fs');
+const config = require('./config');
 const file = `${config.filePath}${config.fileName}`;
-
 
 // The updateRequired module object.
 var updateRequired = {};
 
 // @name checkData
-// @desc Checks a series of conditions to see if the data requires updating. Returns true if it does.
+// @desc Returns true if the file is older than the fileLife variable in config.js or if the files doesn't exist (there is an error)
 updateRequired.checkData = function () {
-  var deferred = Q.defer();
 
-  // Get the date last modified of the file.
-  const {mtime} = fs.statSync(file);
-
-  // Make a request to get  data from the list. Limited to a maximum of 1000 items
-  // without &$top=1000 server-driven paging restricts the output to 200 items.
-  request.get(`https://graph.microsoft.com/v1.0/sites/${config.siteId}/lists/${config.listId}/items?expand=fields&$top=1000`, {
-    auth: {
-      bearer: token
+    try {
+        const {mtime} = fs.statSync(file); // Get file modified date
+        const fileAgeSecs = (Date.now() - mtime.valueOf())/1000;
+        console.log(`File data last modified: ${mtime}`);
+        console.log(`File age in seconds: ${fileAgeSecs}`);
+        if (fileAgeSecs > config.fileLife) return true;
+    } catch(err) {
+        console.log('Its knackered ' + err);
+        return true;
     }
-  }, function (err, response, body) {
-    var parsedBody = JSON.parse(body);
-
-    if (err) {
-      deferred.reject(err);
-    } else if (parsedBody.error) {
-      deferred.reject(parsedBody.error.message);
-    } else {
-      // The value of the body will be an array of the list data.
-      deferred.resolve(parsedBody.value);
-    }
-  });
-
-  return deferred.promise;
-};
-
+}
 
 module.exports = updateRequired;

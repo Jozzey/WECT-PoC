6c9ebf4b47b017a458efb6975015d753355bdfbb
Jozzey <jozzey@hotmail.com>
2020-02-13 13:02:28 +0000

    Updated to use native promises

--- graph.js
+++ graph.js
@@ -1,5 +1,4 @@
-const request = require('request')
-const Q = require('q')
+const rpn = require('request-promise-native')
 const { siteId, listId } = require('./config')
 
 // The graph module object.
@@ -8,54 +7,56 @@ const graph = {}
 // @name getData
 // @desc Makes a request to the Microsoft Graph for all the data in the specified list.
 graph.getData = function (token) {
-  const deferred = Q.defer()
+  return new Promise((resolve, reject) => {
+    // Make a request to get  data from the list. Limited to a maximum of 1000 items
+    // without &$top=1000 server-driven paging restricts the output to 200 items.
+    rpn.get(`https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/items?expand=fields&$top=1000`, {
+      auth: {
+        bearer: token
+      }
+    }).then(getBody, error)
 
-  // Make a request to get data from the list. Limited to a maximum of 1000 items
-  // without &$top=1000 server-driven paging restricts the output to 200 items.
-  request.get(`https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/items?expand=fields&$top=1000`, {
-    auth: {
-      bearer: token
+    function getBody (body) {
+      const parsedBody = JSON.parse(body)
+
+      if (parsedBody.error) {
+        return reject(parsedBody.error.message)
+      } else {
+        // The value of the body will be an array of the list data.
+        return resolve(parsedBody.value)
+      }
     }
-  }, (err, response, body) => {
-    const parsedBody = JSON.parse(body)
-
-    if (err) {
-      deferred.reject(err)
-    } else if (parsedBody.error) {
-      deferred.reject(parsedBody.error.message)
-    } else {
-      // The value of the body will be an array of the list data.
-      deferred.resolve(parsedBody.value)
+    function error (err) {
+      return reject(err)
     }
   })
-
-  return deferred.promise
 }
 
 // @name getColData
 // @desc Makes a request to the Microsoft Graph for a filtered set of the lists column data.
 graph.getColData = function (token) {
-  const deferred = Q.defer()
-
+  return new Promise((resolve, reject) => {
   // Make a request to get column data from the list.
-  request.get(`https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/columns?$select=columnGroup,displayName,name,readOnly`, {
-    auth: {
-      bearer: token
-    }
-  }, (err, response, body) => {
-    const parsedBody = JSON.parse(body)
+    rpn.get(`https://graph.microsoft.com/v1.0/sites/${siteId}/lists/${listId}/columns?$select=columnGroup,displayName,name,readOnly`, {
+      auth: {
+        bearer: token
+      }
+    }).then(getBody, error)
 
-    if (err) {
-      deferred.reject(err)
-    } else if (parsedBody.error) {
-      deferred.reject(parsedBody.error.message)
-    } else {
+    function getBody (body) {
+      const parsedBody = JSON.parse(body)
+
+      if (parsedBody.error) {
+        return reject(parsedBody.error.message)
+      } else {
       // The value of the body will be an array of the column data.
-      deferred.resolve(parsedBody.value)
+        return resolve(parsedBody.value)
+      }
+    }
+    function error (err) {
+      return reject(err)
     }
   })
-
-  return deferred.promise
 }
 
 module.exports = graph
